8185d02fc1f448861c032b9ee3bfd9e11b422db6
oijwef098234 <oijwef098234@gmail.com>
2026-02-19 22:24:52 +0900

    feat : GlobalExceptionFilter 클래스 구현

---
+++ src/main/java/com/command/toyvillage_server/global/error/GlobalExceptionFilter.java
@@ -0,0 +1,56 @@
+package com.command.toyvillage_server.global.error;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class GlobalExceptionFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper;
+
+    private static final String CONTENT_TYPE = "application/json";
+    private static final String CHARACTER_ENCODING = "UTF-8";
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    )throws IOException {
+
+        try {
+            filterChain.doFilter(request,response);
+        } catch (ToyVillageExceptioon e){
+            ErrorCode errorCode = e.getErrorCode();
+            writerErrorResponse(response, errorCode.getStatusCode(), ErrorResponse.of(errorCode, errorCode.getErrorMessage()));
+        } catch (Exception e){
+            log.error("예상하지 못한 에러", e);
+            writerErrorResponse(
+                    response,
+                    500,
+                    ErrorResponse.of(
+                            500,
+                            "예상치 못한 오류가 발생했습니다."
+                    )
+            );
+        }
+    }
+
+    private void writerErrorResponse(HttpServletResponse response, int statusCode, ErrorResponse errorResponse) throws IOException{
+        response.setStatus(statusCode);
+        response.setContentType(CONTENT_TYPE);
+        response.setCharacterEncoding(CHARACTER_ENCODING);
+        objectMapper.writeValue(response.getWriter(), errorResponse);
+    }
+}
