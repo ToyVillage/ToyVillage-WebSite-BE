89f091c0bda84ed801b46020711b217406e6f97f
oijwef098234 <oijwef098234@gmail.com>
2026-02-19 21:42:45 +0900

    setting: 초기 세팅

---
+++ src/main/java/com/command/toyvillage_server/ToyVillageServerApplication.java
@@ -0,0 +1,13 @@
+package com.command.toyvillage_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyVillageServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyVillageServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/command/toyvillage_server/ToyVillageServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.command.toyvillage_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyVillageServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
