e7969c601a5d421521337ab1b0027649c7e7244b
Kanghee <kh63167574@gmail.com>
2026-02-21 20:02:40 +0900

    feat : 이벤트 게시글 전체조회 service 추가

---
+++ src/main/java/com/command/toyvillage_server/domain/event/service/EventQueryAllService.java
@@ -0,0 +1,32 @@
+package com.command.toyvillage_server.domain.event.service;
+
+import com.command.toyvillage_server.domain.event.domain.Event;
+import com.command.toyvillage_server.domain.event.domain.repository.EventRepository;
+import com.command.toyvillage_server.domain.event.presentation.dto.response.EventResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EventQueryAllService {
+    private final EventRepository eventRepository;
+
+    public List<EventResponse> execute() {
+        List<Event> events = eventRepository.findAll();
+
+        List<EventResponse> EventAllResponses = events.stream()
+                .map(event ->
+                        EventResponse.builder()
+                                .event_id(event.getId())
+                                .event_name(event.getTitle())
+                                .event_description(event.getDescription())
+                                .event_start_date(event.getCreatedAt())
+                                .event_end_date(event.getEndedAt())
+                                .event_subjects(event.getSubjects())
+                                .build()
+                ).toList();
+    return EventAllResponses;
+    }
+}
