20d6169714c5ce5d57aa294918cf95de625035ca
Kanghee <kh63167574@gmail.com>
2026-02-21 20:06:35 +0900

    feat: 이벤트 게시글 전체조회 Controller 세팅

--- src/main/java/com/command/toyvillage_server/domain/event/presentation/EventController.java
+++ src/main/java/com/command/toyvillage_server/domain/event/presentation/EventController.java
@@ -1,4 +1,26 @@
 package com.command.toyvillage_server.domain.event.presentation;
 
+import com.command.toyvillage_server.domain.event.presentation.dto.response.EventResponse;
+import com.command.toyvillage_server.domain.event.service.EventQueryAllService;
+import com.command.toyvillage_server.domain.event.service.EventQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/events")
+@RequiredArgsConstructor
 public class EventController {
+    private final EventQueryAllService eventQueryAllService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<EventResponse> getEvents() {
+        return eventQueryAllService.execute();
+    }
 }
