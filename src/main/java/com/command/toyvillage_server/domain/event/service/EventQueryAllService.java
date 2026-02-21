52e553bcfa2e202c51b600fa63859b18390d365d
Kanghee <kh63167574@gmail.com>
2026-02-21 20:10:17 +0900

    refactor : 트랜잭션추가

--- src/main/java/com/command/toyvillage_server/domain/event/service/EventQueryAllService.java
+++ src/main/java/com/command/toyvillage_server/domain/event/service/EventQueryAllService.java
@@ -5,14 +5,17 @@ import com.command.toyvillage_server.domain.event.domain.repository.EventReposit
 import com.command.toyvillage_server.domain.event.presentation.dto.response.EventResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+
 public class EventQueryAllService {
     private final EventRepository eventRepository;
 
+    @Transactional(readOnly = true)
     public List<EventResponse> execute() {
         List<Event> events = eventRepository.findAll();
 
