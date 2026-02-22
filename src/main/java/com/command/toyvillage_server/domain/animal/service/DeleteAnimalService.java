319eed5ff8a8a0d1d82188a3ea5420777aa96649
oijwef098234 <oijwef098234@gmail.com>
2026-02-22 23:55:26 +0900

    feat : 동물 소개 삭제 구현

--- src/main/java/com/command/toyvillage_server/domain/animal/service/DeleteAnimalService.java
+++ src/main/java/com/command/toyvillage_server/domain/animal/service/DeleteAnimalService.java
@@ -2,20 +2,23 @@ package com.command.toyvillage_server.domain.animal.service;
 
 import com.command.toyvillage_server.domain.animal.domain.Animal;
 import com.command.toyvillage_server.domain.animal.domain.repository.AnimalRepository;
-import com.command.toyvillage_server.domain.animal.presentation.dto.response.AnimalResponse;
+import com.command.toyvillage_server.domain.animal.exception.AnimalNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class UpdateAnimalService {
+public class DeleteAnimalService {
     private final AnimalRepository animalRepository;
 
-    public AnimalResponse execute(Long animalId) {
-        animalRepository.findById(animalId)
-                .orElseThrow(() -> );
-    }
+    @Transactional
+    public void execute(Long animalId) {
+        Animal animal = animalRepository.findById(animalId)
+                .orElseThrow(() -> AnimalNotFoundException.EXCEPTION);
 
+        animalRepository.delete(animal);
+    }
 }
