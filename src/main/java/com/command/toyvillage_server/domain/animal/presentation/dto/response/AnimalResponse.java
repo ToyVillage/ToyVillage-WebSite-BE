56f75e671e337a548918b4ec4682d57563c83ccd
oijwef098234 <oijwef098234@gmail.com>
2026-02-22 23:49:30 +0900

    feat : AnimalResponse 구현

---
+++ src/main/java/com/command/toyvillage_server/domain/animal/presentation/dto/response/AnimalResponse.java
@@ -0,0 +1,7 @@
+package com.command.toyvillage_server.domain.animal.presentation.dto.response;
+
+public record AnimalResponse(
+        String animalKind,
+        String animalDescription
+) {
+}
---
+++ src/main/java/com/command/toyvillage_server/domain/animal/service/UpdateAnimalService.java
@@ -0,0 +1,15 @@
+package com.command.toyvillage_server.domain.animal.service;
+
+import com.command.toyvillage_server.domain.animal.domain.repository.AnimalRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UpdateAnimalService {
+    private final AnimalRepository animalRepository;
+
+    public
+}
