648f8e7b7492c04b856cc97af39f8033ccb0f098
treegus <cowtree28@gmail.com>
2026-02-23 10:44:24 +0900

    refactor: 불필요한 import문 삭제

--- src/main/java/com/command/toyvillage_server/domain/faq/service/CreateFaqService.java
+++ src/main/java/com/command/toyvillage_server/domain/faq/service/CreateFaqService.java
@@ -3,7 +3,6 @@ package com.command.toyvillage_server.domain.faq.service;
 import com.command.toyvillage_server.domain.faq.domain.Faq;
 import com.command.toyvillage_server.domain.faq.domain.repository.FaqRepository;
 import com.command.toyvillage_server.domain.faq.presentation.dto.request.FaqCreateRequest;
-import com.command.toyvillage_server.domain.faq.presentation.dto.response.MessageResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
