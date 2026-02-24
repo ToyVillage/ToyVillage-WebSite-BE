09376a9e454196bb735f78126998f5087122fb3b
Kanghee <kh63167574@gmail.com>
2026-02-24 12:27:00 +0900

    feat : news 엔티티 추가

---
+++ src/main/java/com/command/toyvillage_server/domain/news/domain/News.java
@@ -0,0 +1,33 @@
+package com.command.toyvillage_server.domain.news.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class News {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "news_id", nullable = false)
+    private Long id;
+
+    @Column(name = "news_title", nullable = false)
+    private String title;
+
+    @Column(name = "news_description", nullable = false)
+    private String description;
+
+    @CreatedDate
+    @Column(name = "news_postdate",nullable = false)
+    private LocalDateTime createdDate;
+}
