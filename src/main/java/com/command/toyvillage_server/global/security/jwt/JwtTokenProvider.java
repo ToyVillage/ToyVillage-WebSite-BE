ed1175f99442476f6fd489f8bbe23834b8f5511e
oijwef098234 <oijwef098234@gmail.com>
2026-02-26 23:54:42 +0900

    feat : 의존성 추가 및 yml 수정

---
+++ src/main/java/com/command/toyvillage_server/global/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,123 @@
+package com.command.toyvillage_server.global.security.jwt;
+
+import ch.qos.logback.core.net.server.Client;
+import com.command.toyvillage_server.domain.auth.domain.repository.AdminRepository;
+import com.command.toyvillage_server.global.security.auth.CustomUserDetailsService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+
+Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+    private final JwtProperties jwtProperties;
+    private final AdminRepository adminRepository;
+    private final CustomUserDetailsService customUserDetailsService;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    private static final String CLAIM_TYPE = "type";
+    private static final String ACCESS_TYPE = "access";
+    private static final String REFRESH_TYPE = "refresh";
+//    private static final int MILLISECONDS = 1000;
+    private static final long ACCESS_EXPIRATION = 86400L; // 24시간
+//    private static final long ADMIN_ACCESS_EXPIRATION = 7200L; // 2시간
+
+    //access token 생성
+    public String createAccessToken(String adminName) {
+
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setSubject(adminName)
+                .claim(CLAIM_TYPE, ACCESS_TYPE) // 액세스 토큰임을 나타냄
+                .setIssuedAt(now) // 토큰 발행 시간 정보
+                .setExpiration(new Date(now.getTime() + ACCESS_EXPIRATION)) // 토큰의 만료 시간 설정
+                .signWith(SignatureAlgorithm.HS512, jwtProperties.getSecretKey())
+                .compact();
+
+    }
+
+    //refresh token 생성
+    public String createRefreshToken(String organName, Client client) {
+
+        Date now = new Date();
+
+        String refreshToken = Jwts.builder()
+                .claim(CLAIM_TYPE, REFRESH_TYPE)  //refresh 토큰임을 나타냄
+                .setIssuedAt(now)
+                .setExpiration(new java.sql.Timestamp(now.getTime() + jwtProperties.getRefreshExpiration() * MILLISECONDS))
+                .signWith(SignatureAlgorithm.HS512, jwtProperties.getSecretKey())
+                .compact();
+
+
+        refreshTokenRepository.save(
+                RefreshToken.builder()
+                        .organName(organName)
+                        .client(client.name())
+                        .token(refreshToken)
+                        .timeToLive((jwtProperties.getRefreshExpiration()))
+                        .build()
+        );
+
+        return refreshToken;
+    }
+
+    // 토큰에 담겨 있는 userId로 SpringSecurity Authentication 정보를 반환 하는 메서드
+    public Authentication getAuthentication(String token) {
+
+        Claims claims = getClaims(token);
+
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(claims.getSubject());
+
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    public Claims getClaims(String token) {
+
+        try {
+            return Jwts
+                    .parser() //JWT parser 생성
+                    .setSigningKey(jwtProperties.getSecretKey())
+                    .parseClaimsJws(token)
+                    .getBody();
+        }
+        catch (ExpiredJwtException e) {
+            throw  ExpiredTokenException.EXCEPTION;
+        }
+        catch (Exception E) {
+            throw InvalidTokenException.EXCEPTION;
+        }
+    }
+
+    public TokenResponse receiveToken(String organName, Client client) {
+
+        organRepository.findByOrganName(organName)
+                .orElseThrow(() -> OrganNotFoundException.EXCEPTION);
+
+        return TokenResponse.builder()
+                .accessToken(createAccessToken(organName, client))
+                .refreshToken(createRefreshToken(organName, client))
+                .organName(organName)
+                .build();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+
+        String bearerToken = request.getHeader(jwtProperties.getHeader());
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(jwtProperties.getPrefix())
+                && bearerToken.length() > jwtProperties.getPrefix().length() + 1) {
+            return bearerToken.substring(7);
+        }
+
+        return null;
+    }
+}
