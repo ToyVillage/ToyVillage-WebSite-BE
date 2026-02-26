2fd0cbbf8d69e6526dd85fd3865c519675ca5c82
oijwef098234 <oijwef098234@gmail.com>
2026-02-27 00:19:31 +0900

    feat : JwConfig 구현

---
+++ src/main/java/com/command/toyvillage_server/global/config/JwtConfig.java
@@ -0,0 +1,10 @@
+package com.command.toyvillage_server.global.config;
+
+import com.command.toyvillage_server.global.security.jwt.JwtProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(JwtProperties.class)
+public class JwtConfig {
+}
