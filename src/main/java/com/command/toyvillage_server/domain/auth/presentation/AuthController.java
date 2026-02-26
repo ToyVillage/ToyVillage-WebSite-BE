16a61608f316e1e4d7ca547ae1435e5f53a87b85
oijwef098234 <oijwef098234@gmail.com>
2026-02-27 00:46:52 +0900

    feat : controller 매핑

--- src/main/java/com/command/toyvillage_server/domain/auth/presentation/AuthController.java
+++ src/main/java/com/command/toyvillage_server/domain/auth/presentation/AuthController.java
@@ -1,8 +1,15 @@
 package com.command.toyvillage_server.domain.auth.presentation;
 
+import com.command.toyvillage_server.domain.auth.presentation.dto.request.AdminLoginRequest;
+import com.command.toyvillage_server.domain.auth.presentation.dto.response.TokenResponse;
 import com.command.toyvillage_server.domain.auth.service.AdminLoginService;
+import com.command.toyvillage_server.global.util.CookieUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AdminLoginService adminLoginService;
 
+    private final CookieUtil cookieUtil;
+
     @PostMapping("/login")
-    public
+    public ResponseEntity<String> login(
+            @RequestBody AdminLoginRequest request,
+            HttpServletResponse response
+    ){
+        TokenResponse result = adminLoginService.execute(request);
 
+        cookieUtil.addRefreshTokenCookie(response, result.refreshToken());
 
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(result.accessToken());
+    }
 }
