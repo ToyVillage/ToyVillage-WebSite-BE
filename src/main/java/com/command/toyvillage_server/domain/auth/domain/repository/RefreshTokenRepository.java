53bd66e0f97a2ce3f1bc58d7b2cdb344987c6263
oijwef098234 <oijwef098234@gmail.com>
2026-02-27 15:33:21 +0900

    feat : 제네릭 타입 일치

--- src/main/java/com/command/toyvillage_server/domain/auth/domain/repository/RefreshTokenRepository.java
+++ src/main/java/com/command/toyvillage_server/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -3,5 +3,5 @@ package com.command.toyvillage_server.domain.auth.domain.repository;
 import com.command.toyvillage_server.domain.auth.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 }
