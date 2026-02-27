00d49d4fd035d5ffd50bb06298301f5b9aa08905
oijwef098234 <oijwef098234@gmail.com>
2026-02-27 15:27:06 +0900

    feat : @valid어노테이션 추가

--- src/main/java/com/command/toyvillage_server/domain/auth/presentation/dto/request/AdminLoginRequest.java
+++ src/main/java/com/command/toyvillage_server/domain/auth/presentation/dto/request/AdminLoginRequest.java
@@ -1,7 +1,12 @@
 package com.command.toyvillage_server.domain.auth.presentation.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record AdminLoginRequest(
+        @NotBlank(message = "사용자 이름을 공백으로 둘 순 없습니다.")
         String username,
+
+        @NotBlank(message = "비밀번호를 공백으로 둘 순 없습니다.")
         String password
 ) {
 }
