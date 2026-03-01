7d7c351774855c28b503781b5b0e0f309ce916ec
oijwef098234 <oijwef098234@gmail.com>
2026-03-01 17:08:49 +0900

    feat : 회원가입 요청 dto 생성

---
+++ src/main/java/com/command/toyvillage_server/domain/auth/service/AdminSignUpService.java
@@ -0,0 +1,16 @@
+package com.command.toyvillage_server.domain.auth.service;
+
+import com.command.toyvillage_server.domain.auth.domain.Admin;
+import com.command.toyvillage_server.domain.auth.domain.repository.AdminRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AdminSignUpService {
+    private final AdminRepository adminRepository;
+
+    @Transactional
+    public void execute(Admin admin) {}
+}
