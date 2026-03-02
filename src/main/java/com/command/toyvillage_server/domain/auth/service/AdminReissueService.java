96ee5fd3ffb4275a154b04900d68946838ef2325
oijwef098234 <oijwef098234@gmail.com>
2026-03-02 23:02:54 +0900

    feat : reissue 구현

--- src/main/java/com/command/toyvillage_server/domain/auth/service/AdminReissueService.java
+++ src/main/java/com/command/toyvillage_server/domain/auth/service/AdminReissueService.java
@@ -1,7 +1,9 @@
 package com.command.toyvillage_server.domain.auth.service;
 
+import com.command.toyvillage_server.domain.auth.domain.Admin;
 import com.command.toyvillage_server.domain.auth.domain.repository.RefreshTokenRepository;
 import com.command.toyvillage_server.domain.auth.exception.RefreshTokenNotFoundException;
+import com.command.toyvillage_server.domain.auth.facade.AdminFacade;
 import com.command.toyvillage_server.domain.auth.presentation.dto.request.ReissueRequest;
 import com.command.toyvillage_server.domain.auth.presentation.dto.response.TokenResponse;
 import com.command.toyvillage_server.global.security.jwt.JwtTokenProvider;
@@ -13,8 +15,11 @@ import org.springframework.stereotype.Service;
 public class AdminReissueService {
     private final RefreshTokenRepository refreshTokenRepository;
     private final JwtTokenProvider jwtTokenProvider;
+    private final AdminFacade adminFacade;
 
     public TokenResponse execute(ReissueRequest request) {
+        adminFacade.currentAdmin();
+
         refreshTokenRepository.findByUsername(request.username())
                 .orElseThrow(() -> RefreshTokenNotFoundException.EXCEPTION);
 
