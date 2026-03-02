ff0c2029616cd4da1baa75d7cae0961e6f1889f6
oijwef098234 <oijwef098234@gmail.com>
2026-03-02 23:12:13 +0900

    feat : controller 매핑

--- src/main/java/com/command/toyvillage_server/domain/auth/presentation/AuthController.java
+++ src/main/java/com/command/toyvillage_server/domain/auth/presentation/AuthController.java
@@ -3,9 +3,11 @@ package com.command.toyvillage_server.domain.auth.presentation;
 import com.command.toyvillage_server.domain.animal.presentation.dto.response.MessageResponse;
 import com.command.toyvillage_server.domain.auth.presentation.dto.request.AdminLoginRequest;
 import com.command.toyvillage_server.domain.auth.presentation.dto.request.AdminSignUpRequest;
+import com.command.toyvillage_server.domain.auth.presentation.dto.request.ReissueRequest;
 import com.command.toyvillage_server.domain.auth.presentation.dto.response.AccessTokenResponse;
 import com.command.toyvillage_server.domain.auth.presentation.dto.response.TokenResponse;
 import com.command.toyvillage_server.domain.auth.service.AdminLoginService;
+import com.command.toyvillage_server.domain.auth.service.AdminReissueService;
 import com.command.toyvillage_server.domain.auth.service.AdminSignUpService;
 import com.command.toyvillage_server.global.util.CookieUtil;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,10 +15,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -24,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AdminLoginService adminLoginService;
     private final AdminSignUpService adminSignUpService;
+    public final AdminReissueService adminReissueService;
 
     private final CookieUtil cookieUtil;
 
@@ -49,5 +49,19 @@ public class AuthController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(MessageResponse.of("회원가입 완료되었습니다. 로그인 후 이용해주세요."));
+    }
+
+    @PatchMapping("/reissue")
+    public ResponseEntity<AccessTokenResponse> reissue(
+            @RequestBody ReissueRequest request,
+            HttpServletResponse response
+    ){
+        TokenResponse result = adminReissueService.execute(request);
+
+        cookieUtil.addRefreshTokenCookie(response, result.refreshToken());
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(AccessTokenResponse.of(result.accessToken()));
     }
 }
