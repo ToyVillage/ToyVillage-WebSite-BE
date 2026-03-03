dfbee239cbd7a4c7ad767935189eff831d0dd042
oijwef098234 <oijwef098234@gmail.com>
2026-03-03 23:13:01 +0900

    feat : NPE 발생 가능성 코드 수정

--- src/main/java/com/command/toyvillage_server/domain/auth/facade/AdminFacade.java
+++ src/main/java/com/command/toyvillage_server/domain/auth/facade/AdminFacade.java
@@ -4,6 +4,7 @@ import com.command.toyvillage_server.domain.auth.domain.Admin;
 import com.command.toyvillage_server.domain.auth.domain.repository.AdminRepository;
 import com.command.toyvillage_server.domain.auth.exception.AdminNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +14,13 @@ public class AdminFacade {
     private final AdminRepository adminRepository;
 
     public Admin currentAdmin() {
-        String adminName = SecurityContextHolder.getContext().getAuthentication().getName();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null) {
+                throw AdminNotFoundException.EXCEPTION;
+        }
+
+        String adminName = authentication.getName();
 
         return adminRepository.findByUsername(adminName)
                 .orElseThrow(() -> AdminNotFoundException.EXCEPTION);
