1de282714d435a1788b0b1caa8efb77ba352c429
oijwef098234 <oijwef098234@gmail.com>
2026-03-03 22:55:08 +0900

    feat : yml accessToken 만료시간 설정

--- src/main/java/com/command/toyvillage_server/domain/auth/service/AdminReissueService.java
+++ src/main/java/com/command/toyvillage_server/domain/auth/service/AdminReissueService.java
@@ -1,6 +1,5 @@
 package com.command.toyvillage_server.domain.auth.service;
 
-import com.command.toyvillage_server.domain.auth.domain.Admin;
 import com.command.toyvillage_server.domain.auth.domain.RefreshToken;
 import com.command.toyvillage_server.domain.auth.domain.repository.RefreshTokenRepository;
 import com.command.toyvillage_server.domain.auth.exception.RefreshTokenNotFoundException;
