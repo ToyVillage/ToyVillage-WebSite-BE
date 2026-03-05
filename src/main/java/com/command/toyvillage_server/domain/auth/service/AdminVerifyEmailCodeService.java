851c828b12ef2b65b5c623fa4401a97e066cdfae
oijwef098234 <oijwef098234@gmail.com>
2026-03-06 08:51:49 +0900

    feat : 비밀번호 변경 권한 Entity 구현

---
+++ src/main/java/com/command/toyvillage_server/domain/auth/service/AdminVerifyEmailCodeService.java
@@ -0,0 +1,10 @@
+package com.command.toyvillage_server.domain.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AdminVerifyEmailCodeService {
+
+}
