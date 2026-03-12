8f65da291ee76afc078bed9b12ed19c3cdb6ebcc
oijwef098234 <oijwef098234@gmail.com>
2026-03-13 08:40:03 +0900

    feat : 이메일 형식 어노테이션 추가

--- src/main/java/com/command/toyvillage_server/domain/auth/presentation/dto/request/AdminSignUpRequest.java
+++ src/main/java/com/command/toyvillage_server/domain/auth/presentation/dto/request/AdminSignUpRequest.java
@@ -1,8 +1,10 @@
 package com.command.toyvillage_server.domain.auth.presentation.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record AdminSignUpRequest(
+        @Email(message = "올바른 이메일 형식으로 입력해주세요.")
         @NotBlank(message = "이메일을 공백으로 둘 순 없습니다.")
         String email,
 
