86a7b7bf5e2f8370a200d7521e3d0b0ad21d2731
oijwef098234 <oijwef098234@gmail.com>
2026-04-13 20:25:41 +0900

    feat : 팝업 레포지토리 구현

---
+++ src/main/java/com/command/toyvillage_server/domain/popup/domain/repository/PopUpRepository.java
@@ -0,0 +1,7 @@
+package com.command.toyvillage_server.domain.popup.domain.repository;
+
+import com.command.toyvillage_server.domain.popup.domain.PopUp;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PopUpRepository extends JpaRepository<PopUp,Long> {
+}
