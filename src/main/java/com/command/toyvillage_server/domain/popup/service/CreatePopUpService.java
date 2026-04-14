90850bb052b4e2551422af1cedd2939fdab7f89f
oijwef098234 <oijwef098234@gmail.com>
2026-04-14 23:38:21 +0900

    feat : 팝업 생성 service 구현

--- src/main/java/com/command/toyvillage_server/domain/popup/service/CreatePopUpService.java
+++ src/main/java/com/command/toyvillage_server/domain/popup/service/CreatePopUpService.java
@@ -1,6 +1,8 @@
 package com.command.toyvillage_server.domain.popup.service;
 
+import com.command.toyvillage_server.domain.popup.domain.PopUp;
 import com.command.toyvillage_server.domain.popup.domain.repository.PopUpRepository;
+import com.command.toyvillage_server.domain.popup.presentation.dto.request.PopUpRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +11,14 @@ import org.springframework.stereotype.Service;
 public class CreatePopUpService {
     private final PopUpRepository popUpRepository;
 
-    public void execute(){
-
+    public void execute(PopUpRequest popUpRequest) {
+        PopUp popUp = PopUp.builder()
+                .content(popUpRequest.content())
+                .contentType(popUpRequest.contentType())
+                .expirationDate(popUpRequest.expirationDate())
+                .priority(popUpRequest.priority())
+                .build();
+        
+        popUpRepository.save(popUp);
     }
 }
