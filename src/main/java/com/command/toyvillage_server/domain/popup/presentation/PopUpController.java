7a62eb5fc1dc7634cdc36d4eb76bd0d096a96616
oijwef098234 <oijwef098234@gmail.com>
2026-04-15 21:13:54 +0900

    feat : 팝업 전체조회 controller 매핑

--- src/main/java/com/command/toyvillage_server/domain/popup/presentation/PopUpController.java
+++ src/main/java/com/command/toyvillage_server/domain/popup/presentation/PopUpController.java
@@ -2,15 +2,14 @@ package com.command.toyvillage_server.domain.popup.presentation;
 
 import com.command.toyvillage_server.domain.popup.presentation.dto.request.PopUpRequest;
 import com.command.toyvillage_server.domain.popup.presentation.dto.response.PopUpResponse;
-import com.command.toyvillage_server.domain.popup.service.CreatePopUpService;
-import com.command.toyvillage_server.domain.popup.service.DeletePopUpService;
-import com.command.toyvillage_server.domain.popup.service.QueryPopUpService;
-import com.command.toyvillage_server.domain.popup.service.UpdatePopUpService;
+import com.command.toyvillage_server.domain.popup.service.*;
 import com.command.toyvillage_server.global.common.response.MessageResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/popup")
@@ -20,6 +19,7 @@ public class PopUpController {
     private final UpdatePopUpService updatePopUpService;
     private final DeletePopUpService deletePopUpService;
     private final QueryPopUpService queryPopUpService;
+    private final QueryListPopUpService queryListPopUpService;
 
     @PostMapping
     public ResponseEntity<MessageResponse> createPopUp(@RequestBody PopUpRequest popUpRequest) {
@@ -58,5 +58,10 @@ public class PopUpController {
             @PathVariable("popup-id")   Long popUpId
     ){
         return queryPopUpService.execute(popUpId);
+    }
+
+    @GetMapping()
+    public List<PopUpResponse> getPopUps(){
+        return queryListPopUpService.execute();
     }
 }
