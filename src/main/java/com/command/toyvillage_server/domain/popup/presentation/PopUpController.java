71238b608a24998e16c35ecf3c367d471430a5aa
oijwef098234 <oijwef098234@gmail.com>
2026-04-15 10:32:11 +0900

    feat : 팝업 수정 기능 controller 매핑

--- src/main/java/com/command/toyvillage_server/domain/popup/presentation/PopUpController.java
+++ src/main/java/com/command/toyvillage_server/domain/popup/presentation/PopUpController.java
@@ -2,26 +2,38 @@ package com.command.toyvillage_server.domain.popup.presentation;
 
 import com.command.toyvillage_server.domain.popup.presentation.dto.request.PopUpRequest;
 import com.command.toyvillage_server.domain.popup.service.CreatePopUpService;
+import com.command.toyvillage_server.domain.popup.service.UpdatePopUpService;
 import com.command.toyvillage_server.global.common.response.MessageResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/popup")
 @RequiredArgsConstructor
 public class PopUpController {
     private final CreatePopUpService createPopUpService;
+    private final UpdatePopUpService updatePopUpService;
 
     @PostMapping
-    public ResponseEntity<MessageResponse> createPopUp(PopUpRequest popUpRequest) {
+    public ResponseEntity<MessageResponse> createPopUp(@RequestBody PopUpRequest popUpRequest) {
         createPopUpService.execute(popUpRequest);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(MessageResponse.of("팝업이 생성되었습니다."));
+    }
+
+    @PatchMapping("/{popup-id}")
+    public ResponseEntity<MessageResponse> updatePopUp(
+            @RequestBody PopUpRequest popUpRequest,
+            @PathVariable("popup-id") Long popUpId
+    ) {
+        updatePopUpService.execute(popUpRequest, popUpId);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(MessageResponse.of("팝업이 수정되었습니다."));
     }
 }
