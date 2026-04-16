2af459db78b50df38e2f26bdc00e215ccf819ba5
oijwef098234 <oijwef098234@gmail.com>
2026-04-16 22:37:17 +0900

    feat : 팝업 request dto validation 어노테이션 추가

--- src/main/java/com/command/toyvillage_server/domain/popup/presentation/dto/request/PopUpRequest.java
+++ src/main/java/com/command/toyvillage_server/domain/popup/presentation/dto/request/PopUpRequest.java
@@ -1,13 +1,22 @@
 package com.command.toyvillage_server.domain.popup.presentation.dto.request;
 
 import com.command.toyvillage_server.domain.popup.domain.enums.ContentType;
+import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDate;
 
 public record PopUpRequest(
+
+        @NotBlank(message = "본문을 비워둘 순 없습니다.")
         String content,
+
+        @NotBlank(message = "본문의 형식을 지정해주세요.")
         ContentType contentType,
+
+        @NotBlank(message = "팝업 마감기한을 설정해주세요.")
         LocalDate expirationDate,
+
+        @NotBlank(message = "팝업의 우선순위를 선택해주세요.")
         Integer priority
 ) {
 }
