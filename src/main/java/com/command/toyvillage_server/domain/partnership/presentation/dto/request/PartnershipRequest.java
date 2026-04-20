b5c9cc09fb58bb71b0534673d09f420f283345ca
Kanghee <kh63167574@gmail.com>
2026-04-20 09:27:50 +0900

    feat : 제약조건 추가

--- src/main/java/com/command/toyvillage_server/domain/partnership/presentation/dto/request/PartnershipRequest.java
+++ src/main/java/com/command/toyvillage_server/domain/partnership/presentation/dto/request/PartnershipRequest.java
@@ -4,6 +4,7 @@ import com.command.toyvillage_server.domain.partnership.domain.PartnershipType;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -16,9 +17,11 @@ public class PartnershipRequest {
     private PartnershipType partnershipType;
 
     @NotBlank(message = "제휴 제목은 공백일 수 없습니다.")
+    @Size(max = 50, message = "제목은 50자를 초과할 수 없습니다.")
     private String title;
 
     @NotBlank(message = "이름은 공백일 수 없습니다.")
+    @Size(max = 50, message = "이름은 50자를 초과할 수 없습니다.")
     private String name;
 
     @Email
@@ -26,6 +29,7 @@ public class PartnershipRequest {
     private String email;
 
     @NotBlank(message = "전화번호는 공백일 수 없습니다.")
+    @Size(max = 15, message = "전화번호는 15자를 초과할 수 없습니다.")
     private String phoneNumber;
 
     @NotBlank(message = "제휴 내용은 공백일 수 없습니다.")
