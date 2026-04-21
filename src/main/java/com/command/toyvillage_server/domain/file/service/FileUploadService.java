720a4521377aecce6712283dffcabc5d6644b208
treegus <cowtree28@gmail.com>
2026-04-22 08:38:33 +0900

    fix: file 저장하지 않는 문제 수정

--- src/main/java/com/command/toyvillage_server/domain/file/service/FileUploadService.java
+++ src/main/java/com/command/toyvillage_server/domain/file/service/FileUploadService.java
@@ -1,11 +1,14 @@
 package com.command.toyvillage_server.domain.file.service;
 
+import com.command.toyvillage_server.domain.file.domain.File;
+import com.command.toyvillage_server.domain.file.domain.repository.FileRepository;
 import com.command.toyvillage_server.domain.file.presentation.dto.response.FileUploadResponse;
 import com.command.toyvillage_server.global.aws.s3.AwsS3Provider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
@@ -13,11 +16,19 @@ import org.springframework.web.multipart.MultipartFile;
 @Slf4j
 public class FileUploadService {
     private final AwsS3Provider awsS3Provider;
+    private final FileRepository fileRepository;
 
     @Transactional
     public FileUploadResponse execute(MultipartFile file) {
         String key = awsS3Provider.upload(file);
 
+        File savedFile = fileRepository.save(
+            File.builder()
+            .fileKey(key)
+            .fileName(StringUtils.getFilename(file.getOriginalFilename()))
+            .build()
+        );
+        log.info("파일{}", savedFile.getId());
         log.info("파일 업로드 완료 / key : {}", key);
         return FileUploadResponse.builder()
             .fileKey(key)
