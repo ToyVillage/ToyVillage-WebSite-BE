16fbec65fd92bdadd7faf4d1292340a324acd1e6
treegus <cowtree28@gmail.com>
2026-04-21 11:27:26 +0900

    feat: 사용되지 않는 AwsS3Provider 의존 삭제 및 정적 팩토리 메서드 형태로 변환

--- src/main/java/com/command/toyvillage_server/domain/gallery/presentation/dto/response/GalleryResponse.java
+++ src/main/java/com/command/toyvillage_server/domain/gallery/presentation/dto/response/GalleryResponse.java
@@ -1,9 +1,11 @@
 package com.command.toyvillage_server.domain.gallery.presentation.dto.response;
 
+import com.command.toyvillage_server.domain.gallery.domain.Gallery;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
 import lombok.Builder;
 
-@Builder
+@Builder(access = AccessLevel.PRIVATE)
 public record GalleryResponse (
     @JsonProperty("gallery_id")
     Long galleryId,
@@ -11,4 +13,12 @@ public record GalleryResponse (
     String galleryTitle,
     @JsonProperty("gallery_file_key")
     String galleryFileKey
-) { }
+) {
+    public static GalleryResponse from(Gallery gallery) {
+        return GalleryResponse.builder()
+            .galleryId(gallery.getId())
+            .galleryTitle(gallery.getTitle())
+            .galleryFileKey(gallery.getFile().getFileKey())
+            .build();
+    }
+}
