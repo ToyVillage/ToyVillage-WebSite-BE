d4f05a9dcb4b58feb6d1c7ec0216d3d61a931051
oijwef098234 <oijwef098234@gmail.com>
2026-04-21 11:24:26 +0900

    refactor : 동물소개 추가 request DTO s3 이미지 필드 추가

--- src/main/java/com/command/toyvillage_server/domain/animal/presentation/dto/request/AnimalRequest.java
+++ src/main/java/com/command/toyvillage_server/domain/animal/presentation/dto/request/AnimalRequest.java
@@ -1,11 +1,15 @@
 package com.command.toyvillage_server.domain.animal.presentation.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import org.springframework.web.multipart.MultipartFile;
 
 public record AnimalRequest(
 
         @NotBlank(message = "동물 종류를 비워둘 수 없습니다.")
         String animalKind,
+
+        @NotBlank(message = "동물 사진을 첨부해주세요.")
+        MultipartFile  animalImage,
 
         @NotBlank(message = "동물 소개를 비워둘 수 없습니다.")
         String animalDescription
