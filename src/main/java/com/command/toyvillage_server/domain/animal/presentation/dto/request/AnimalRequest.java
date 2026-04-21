c18c8a59408cadf6e16a1ddeeec41dd883a51f48
oijwef098234 <oijwef098234@gmail.com>
2026-04-21 21:54:47 +0900

    feat : json 스네이크 케이스로 변경

--- src/main/java/com/command/toyvillage_server/domain/animal/presentation/dto/request/AnimalRequest.java
+++ src/main/java/com/command/toyvillage_server/domain/animal/presentation/dto/request/AnimalRequest.java
@@ -1,17 +1,21 @@
 package com.command.toyvillage_server.domain.animal.presentation.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.web.multipart.MultipartFile;
 
 public record AnimalRequest(
 
         @NotBlank(message = "동물 종류를 비워둘 수 없습니다.")
+        @JsonProperty("animal_kind")
         String animalKind,
 
         @NotBlank(message = "동물 사진을 첨부해주세요.")
+        @JsonProperty("animal_image")
         MultipartFile  animalImage,
 
         @NotBlank(message = "동물 소개를 비워둘 수 없습니다.")
+        @JsonProperty("animal_description")
         String animalDescription
 ) {
 }
--- src/main/java/com/command/toyvillage_server/domain/animal/presentation/dto/response/AnimalResponse.java
+++ src/main/java/com/command/toyvillage_server/domain/animal/presentation/dto/response/AnimalResponse.java
@@ -1,10 +1,17 @@
 package com.command.toyvillage_server.domain.animal.presentation.dto.response;
 
 import com.command.toyvillage_server.domain.animal.domain.Animal;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record AnimalResponse(
+
+        @JsonProperty("animal_kind")
         String animalKind,
+
+        @JsonProperty("animal_image")
         String animalImage,
+
+        @JsonProperty("animal_description")
         String animalDescription
 ) {
     public static AnimalResponse from(Animal animal){
