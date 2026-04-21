381cbb7d75b7563d801a4d8cc1dfde2db02e0326
oijwef098234 <oijwef098234@gmail.com>
2026-04-21 11:27:04 +0900

    refactor : 동물 조회 DTO 에 animalImage 필드 추가

--- src/main/java/com/command/toyvillage_server/domain/animal/presentation/dto/response/AnimalResponse.java
+++ src/main/java/com/command/toyvillage_server/domain/animal/presentation/dto/response/AnimalResponse.java
@@ -4,11 +4,13 @@ import com.command.toyvillage_server.domain.animal.domain.Animal;
 
 public record AnimalResponse(
         String animalKind,
+        String animalImage,
         String animalDescription
 ) {
     public static AnimalResponse from(Animal animal){
         return new AnimalResponse(
                 animal.getAnimalKind(),
+                animal.getAnimalImage(),
                 animal.getAnimalDescription()
         );
     }
