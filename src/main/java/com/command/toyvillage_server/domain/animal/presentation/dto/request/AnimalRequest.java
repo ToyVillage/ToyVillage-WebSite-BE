7076bf53df45a19ac56fedf12ee8fbdc1dc9d010
oijwef098234 <oijwef098234@gmail.com>
2026-04-23 00:19:34 +0900

    feat : AnimalRequest dto에 animalType, popularAnimal필드 추가

--- src/main/java/com/command/toyvillage_server/domain/animal/presentation/dto/request/AnimalRequest.java
+++ src/main/java/com/command/toyvillage_server/domain/animal/presentation/dto/request/AnimalRequest.java
@@ -1,7 +1,9 @@
 package com.command.toyvillage_server.domain.animal.presentation.dto.request;
 
+import com.command.toyvillage_server.domain.animal.domain.enums.AnimalType;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.web.multipart.MultipartFile;
 
 public record AnimalRequest(
@@ -16,6 +18,14 @@ public record AnimalRequest(
 
         @NotBlank(message = "동물 소개를 비워둘 수 없습니다.")
         @JsonProperty("animal_description")
-        String animalDescription
+        String animalDescription,
+
+        @NotNull(message = "동물 종류를 비워둘 수 없습니다.")
+        @JsonProperty("animal_type")
+        AnimalType animalType,
+
+        @NotNull(message = "인기 동물 여부를 선택해주세요.")
+        @JsonProperty("popular_animal")
+        boolean popularAnimal
 ) {
 }
