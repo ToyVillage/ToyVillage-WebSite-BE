fdf97112f7b11d21a44eaadb191c4c180a15d23e
oijwef098234 <oijwef098234@gmail.com>
2026-04-23 01:07:22 +0900

    feat : 동물소개 전체조회 dto json 형식 수정

--- src/main/java/com/command/toyvillage_server/domain/animal/presentation/dto/response/AnimalListResponse.java
+++ src/main/java/com/command/toyvillage_server/domain/animal/presentation/dto/response/AnimalListResponse.java
@@ -1,12 +1,23 @@
 package com.command.toyvillage_server.domain.animal.presentation.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public record AnimalListResponse(
+        @JsonProperty("popular_animal")
         List<AnimalResponse> popularAnimal,
+
+        @JsonProperty("mammals")
         List<AnimalResponse> mammals,
+
+        @JsonProperty("reptiles")
         List<AnimalResponse> reptiles,
+
+        @JsonProperty("fish")
         List<AnimalResponse> fish,
+
+        @JsonProperty("birds")
         List<AnimalResponse> birds
 ) {
     public static AnimalListResponse from(
--- src/main/java/com/command/toyvillage_server/domain/animal/service/QueryAllAnimalService.java
+++ src/main/java/com/command/toyvillage_server/domain/animal/service/QueryAllAnimalService.java
@@ -2,6 +2,7 @@ package com.command.toyvillage_server.domain.animal.service;
 
 import com.command.toyvillage_server.domain.animal.domain.Animal;
 import com.command.toyvillage_server.domain.animal.domain.repository.AnimalRepository;
+import com.command.toyvillage_server.domain.animal.presentation.dto.response.AnimalListResponse;
 import com.command.toyvillage_server.domain.animal.presentation.dto.response.AnimalResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,8 +17,10 @@ public class QueryAllAnimalService {
     private final AnimalRepository animalRepository;
 
     @Transactional(readOnly = true)
-    public List<AnimalResponse> execute() {
+    public AnimalListResponse execute() {
         List<Animal> animals = animalRepository.findAll();
+
+
 
         return animals
                 .stream()
