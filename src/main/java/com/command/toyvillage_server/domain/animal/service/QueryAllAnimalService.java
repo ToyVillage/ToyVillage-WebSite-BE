627dc06a538b986d5c9281c7e846210f71cdad24
oijwef098234 <oijwef098234@gmail.com>
2026-04-24 18:01:55 +0900

    feat : 동물소개 전체조회 로직 변경

--- src/main/java/com/command/toyvillage_server/domain/animal/service/QueryAllAnimalService.java
+++ src/main/java/com/command/toyvillage_server/domain/animal/service/QueryAllAnimalService.java
@@ -1,6 +1,7 @@
 package com.command.toyvillage_server.domain.animal.service;
 
 import com.command.toyvillage_server.domain.animal.domain.Animal;
+import com.command.toyvillage_server.domain.animal.domain.enums.AnimalType;
 import com.command.toyvillage_server.domain.animal.domain.repository.AnimalRepository;
 import com.command.toyvillage_server.domain.animal.presentation.dto.response.AnimalListResponse;
 import com.command.toyvillage_server.domain.animal.presentation.dto.response.AnimalResponse;
@@ -8,7 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,11 +23,31 @@ public class QueryAllAnimalService {
     public AnimalListResponse execute() {
         List<Animal> animals = animalRepository.findAll();
 
-
-
-        return animals
-                .stream()
+        List<AnimalResponse> popularAnimal = animals.stream()
+                .filter(animal -> animal.isPopularAnimal())
                 .map(AnimalResponse::from)
-                .collect(Collectors.toList());
+                .toList();
+
+        Map<AnimalType, List<AnimalResponse>> groupedAnimals = animals.stream()
+                .map(AnimalResponse::from)
+                .collect(Collectors.groupingBy(animalResponse ->
+                        AnimalType.valueOf(animalResponse.animalType().getType())
+                ));
+
+        List<AnimalResponse> mammals = groupedAnimals.getOrDefault(AnimalType.MAMMALS, Collections.emptyList());
+
+        List<AnimalResponse> reptiles = groupedAnimals.getOrDefault(AnimalType.REPTILES, Collections.emptyList());
+
+        List<AnimalResponse> fish = groupedAnimals.getOrDefault(AnimalType.FISH, Collections.emptyList());
+
+        List<AnimalResponse> birds = groupedAnimals.getOrDefault(AnimalType.BIRDS, Collections.emptyList());
+
+        return AnimalListResponse.from(
+                popularAnimal,
+                mammals,
+                reptiles,
+                fish,
+                birds
+        );
     }
 }
