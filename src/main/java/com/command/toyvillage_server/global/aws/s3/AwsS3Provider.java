289e7a1a895459e4bc50db78b75726418e73c9ba
treegus <cowtree28@gmail.com>
2026-05-04 23:26:29 +0900

    feat : 고아 객체 삭제하는 scheduler 구현

--- src/main/java/com/command/toyvillage_server/global/aws/s3/AwsS3Provider.java
+++ src/main/java/com/command/toyvillage_server/global/aws/s3/AwsS3Provider.java
@@ -1,5 +1,6 @@
 package com.command.toyvillage_server.global.aws.s3;
 
+import com.command.toyvillage_server.domain.file.domain.repository.FileRepository;
 import com.command.toyvillage_server.global.aws.s3.exception.FileDeleteFailException;
 import com.command.toyvillage_server.global.aws.s3.exception.FileEmptyException;
 import com.command.toyvillage_server.global.aws.s3.exception.FileUploadFailException;
@@ -12,9 +13,10 @@ import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.exception.SdkException;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.*;
+
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -22,6 +24,7 @@ import java.util.UUID;
 @Slf4j
 public class AwsS3Provider {
     private final S3Client s3Client;
+    private final FileRepository fileRepository;
 
     @Value("${spring.cloud.aws.s3.bucket}")
     private String bucket;
@@ -67,4 +70,32 @@ public class AwsS3Provider {
         }
     }
 
+    public void cleanupOrphanedObjects() {
+        ListObjectsV2Request listObjectsRequest = ListObjectsV2Request.builder()
+            .bucket(bucket)
+            .build();
+
+        ListObjectsV2Response listObjectsV2Response = s3Client.listObjectsV2(listObjectsRequest);
+
+        List<String> keys = listObjectsV2Response.contents().stream()
+            .map(S3Object::key)
+            .toList();
+
+        List<String> dbKeys = fileRepository.findAllFileKeys();
+
+        List<ObjectIdentifier> deleteObjects = keys.stream()
+            .filter(key -> !dbKeys.contains(key))
+            .map(key -> ObjectIdentifier.builder().key(key).build())
+            .toList();
+
+        if (!deleteObjects.isEmpty()) {
+            DeleteObjectsRequest deleteObjectsRequest = DeleteObjectsRequest.builder()
+                .bucket(bucket)
+                .delete(Delete.builder().objects(deleteObjects).build())
+                .build();
+
+            s3Client.deleteObjects(deleteObjectsRequest);
+            System.out.println(deleteObjects.size() + " 개의 고아 객체가 삭제되었습니다.");
+        }
+    }
 }
