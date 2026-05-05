204e701f818737c012de073c38c1647205b9602e
oijwef098234 <oijwef098234@gmail.com>
2026-05-05 23:04:36 +0900

    fix : 불필요한 어노테이션 삭제

--- src/main/java/com/command/toyvillage_server/domain/popup/presentation/dto/request/PopUpRequest.java
+++ src/main/java/com/command/toyvillage_server/domain/popup/presentation/dto/request/PopUpRequest.java
@@ -1,7 +1,6 @@
 package com.command.toyvillage_server.domain.popup.presentation.dto.request;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
@@ -9,7 +8,6 @@ import java.time.LocalDate;
 public record PopUpRequest(
 
         @NotNull(message = "본문을 비워둘 순 없습니다.")
-        @Size(max = 50)
         MultipartFile popupImage,
 
         @NotNull(message = "팝업 마감기한을 설정해주세요.")
