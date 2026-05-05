59f317e4a937196729ad9fa26f1e7204a26a78b5
oijwef098234 <oijwef098234@gmail.com>
2026-05-05 23:05:29 +0900

    fix : 불필요한 import 문 삭제

--- src/main/java/com/command/toyvillage_server/domain/popup/domain/PopUp.java
+++ src/main/java/com/command/toyvillage_server/domain/popup/domain/PopUp.java
@@ -2,7 +2,6 @@ package com.command.toyvillage_server.domain.popup.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 
