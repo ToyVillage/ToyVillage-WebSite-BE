2de77cd4851fb3ab99340b14ce153bf39aab2284
treegus <cowtree28@gmail.com>
2026-05-05 20:24:24 +0900

    fix : 삭제 실패시 예외처리

--- src/main/java/com/command/toyvillage_server/global/aws/s3/AwsS3Provider.java
+++ src/main/java/com/command/toyvillage_server/global/aws/s3/AwsS3Provider.java
@@ -95,9 +95,14 @@ public class AwsS3Provider {
                 .bucket(bucket)
                 .delete(Delete.builder().objects(deleteObjects).build())
                 .build();
-
-            s3Client.deleteObjects(deleteObjectsRequest);
-            System.out.println(deleteObjects.size() + " 개의 고아 객체가 삭제되었습니다.");
+            
+            try {
+                s3Client.deleteObjects(deleteObjectsRequest);
+                log.info("{} 개의 고아 객체가 삭제되었습니다.", deleteObjects.size());
+            } catch (SdkException e) {
+                log.error("고아 객체 삭제 실패");
+                throw FileDeleteFailException.EXCEPTION;
+            }
         }
     }
 }
