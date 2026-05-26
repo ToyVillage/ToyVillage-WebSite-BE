e773563370262ab436a6da118e5fced70479a137
Kanghee <kh63167574@gmail.com>
2026-05-27 02:10:03 +0900

    fix : event 상세조회 수정

--- src/main/java/com/command/toyvillage_server/domain/event/domain/repository/EventRepository.java
+++ src/main/java/com/command/toyvillage_server/domain/event/domain/repository/EventRepository.java
@@ -6,7 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EventRepository extends JpaRepository<Event, Long> {
     @EntityGraph(attributePaths = "file")
     Page<Event> findAll(Pageable pageable);
+
+    @EntityGraph(attributePaths = "file")
+    Optional<Event> findById(Long id);
 }
