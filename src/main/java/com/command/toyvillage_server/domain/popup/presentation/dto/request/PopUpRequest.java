45034de8bf5c61bb8432f7d7883ff22c8a001389
oijwef098234 <oijwef098234@gmail.com>
2026-05-27 15:42:38 +0900

    hotfix :: 필드명 스네이크 케이스로 변경

--- src/main/java/com/command/toyvillage_server/domain/popup/presentation/dto/request/PopUpRequest.java
+++ src/main/java/com/command/toyvillage_server/domain/popup/presentation/dto/request/PopUpRequest.java
@@ -1,5 +1,6 @@
 package com.command.toyvillage_server.domain.popup.presentation.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -13,6 +14,8 @@ public record PopUpRequest(
         String fileKey,
 
         @NotNull(message = "팝업 마감기한을 설정해주세요.")
+        @JsonProperty("expiration_date")
+        @JsonFormat(pattern = "yyyy-MM-dd")
         LocalDate expirationDate,
 
         @NotNull(message = "팝업의 우선순위를 선택해주세요.")
