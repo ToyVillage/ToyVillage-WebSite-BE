a74b485f2cba7d44ca531eba6a694c587f9ec47c
oijwef098234 <oijwef098234@gmail.com>
2026-07-05 00:07:09 +0900

    feat(#73) :: 공지사항 enum 값 추가

--- src/main/java/com/command/toyvillage_server/domain/app/notice/domain/Kind.java
+++ src/main/java/com/command/toyvillage_server/domain/app/notice/domain/Kind.java
@@ -1,4 +1,14 @@
 package com.command.toyvillage_server.domain.app.notice.domain;
 
+import lombok.Getter;
+
 public enum Kind {
+    ALL("전체");
+
+    @Getter
+    private final String kindName;
+
+    Kind(String kindName) {
+        this.kindName = kindName;
+    }
 }
