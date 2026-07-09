e0f9955f4324f5ea3bce494801250916f8e78ae9
treegus <cowtree28@gmail.com>
2026-07-09 20:56:11 +0900

    feat: 자료실 entity 추가

---
+++ src/main/java/com/command/toyvillage_server/domain/app/document/domain/Document.java
@@ -0,0 +1,54 @@
+package com.command.toyvillage_server.domain.app.document.domain;
+
+import com.command.toyvillage_server.domain.web.file.domain.File;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table
+@EntityListeners(AuditingEntityListener.class)
+public class Document {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "document_id")
+    private Long id;
+
+    @Column(name = "document_title", length = 100, nullable = false)
+    private String title;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "document_type", nullable = false)
+    private DocumentType type;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @OneToMany
+    @JoinTable(
+        name = "tbl_document_file",
+        joinColumns = @JoinColumn(name = "document_id"),
+        inverseJoinColumns = @JoinColumn(name = "file_id", unique = true)
+    )
+    private List<File> files;
+
+    @Builder
+    public Document(String title, DocumentType type, List<File> files) {
+        this.title = title;
+        this.type = type;
+        this.files = files;
+    }
+}
+
+
+
