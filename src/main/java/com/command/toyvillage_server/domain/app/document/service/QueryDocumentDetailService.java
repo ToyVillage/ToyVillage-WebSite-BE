85a2ab35953fd0fb1803e1c45bb05d5cff0317ab
treegus <cowtree28@gmail.com>
2026-07-12 18:12:03 +0900

    feat : 빠진 예외 추가

--- src/main/java/com/command/toyvillage_server/domain/app/document/service/QueryDocumentDetailService.java
+++ src/main/java/com/command/toyvillage_server/domain/app/document/service/QueryDocumentDetailService.java
@@ -2,6 +2,7 @@ package com.command.toyvillage_server.domain.app.document.service;
 
 import com.command.toyvillage_server.domain.app.document.domain.Document;
 import com.command.toyvillage_server.domain.app.document.domain.repository.DocumentRepository;
+import com.command.toyvillage_server.domain.app.document.exception.DocumentNotFoundException;
 import com.command.toyvillage_server.domain.app.document.presentation.dto.response.DocumentDetailResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,7 @@ public class QueryDocumentDetailService {
 
     @Transactional(readOnly = true)
     public DocumentDetailResponse execute(Long id) {
-        Document document = documentRepository.findById(id).orElseThrow();
+        Document document = documentRepository.findById(id).orElseThrow(() -> DocumentNotFoundException.EXCEPTION);
 
         return DocumentDetailResponse.from(document);
     }
