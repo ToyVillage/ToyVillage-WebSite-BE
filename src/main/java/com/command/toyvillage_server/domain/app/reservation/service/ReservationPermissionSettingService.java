785405352d9d5d1465b73b75da3d909db7dd959a
oijwef098234 <oijwef098234@gmail.com>
2026-07-18 02:47:44 +0900

    feat(#82) :: 유저 권한 부여 메서드 구현

--- src/main/java/com/command/toyvillage_server/domain/app/reservation/service/ReservationPermissionSettingService.java
+++ src/main/java/com/command/toyvillage_server/domain/app/reservation/service/ReservationPermissionSettingService.java
@@ -1,6 +1,8 @@
 package com.command.toyvillage_server.domain.app.reservation.service;
 
+import com.command.toyvillage_server.domain.app.user.domain.User;
 import com.command.toyvillage_server.domain.app.user.domain.repository.UserRepository;
+import com.command.toyvillage_server.domain.app.user.exception.UserNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,8 +13,10 @@ public class ReservationPermissionSettingService {
     private final UserRepository userRepository;
 
     @Transactional
-    public void execute(Long userId) {
-        userRepository.findById(userId)
-            .orElseThrow();
+    public void execute(Long userId, boolean permission) {
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+
+        user.setReservationPermission(permission);
     }
 }
--- src/main/java/com/command/toyvillage_server/domain/app/user/domain/User.java
+++ src/main/java/com/command/toyvillage_server/domain/app/user/domain/User.java
@@ -2,11 +2,13 @@ package com.command.toyvillage_server.domain.app.user.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "tbl_user")
 public class User {
@@ -22,4 +24,8 @@ public class User {
 
     @Column(nullable = false)
     private boolean reservationPermission;
+
+    public void setReservationPermission(boolean reservationPermission) {
+        this.reservationPermission = reservationPermission;
+    }
 }
