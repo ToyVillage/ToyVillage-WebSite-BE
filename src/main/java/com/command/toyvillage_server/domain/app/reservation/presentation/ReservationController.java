10fe4602305baee96a6d9dd6b87495decc5bdf22
oijwef098234 <oijwef098234@gmail.com>
2026-07-18 00:41:23 +0900

    feat(#82) :: 단체예약 전체조회 메서드 구현

--- src/main/java/com/command/toyvillage_server/domain/app/reservation/presentation/ReservationController.java
+++ src/main/java/com/command/toyvillage_server/domain/app/reservation/presentation/ReservationController.java
@@ -1,5 +1,7 @@
 package com.command.toyvillage_server.domain.app.reservation.presentation;
 
+import com.command.toyvillage_server.domain.app.reservation.service.ReservationQueryListService;
+import com.command.toyvillage_server.domain.app.reservation.service.ReservationQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,4 +9,8 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/Reservation")
 public class ReservationController {
+    private final ReservationQueryService reservationQueryService;
+    private final ReservationQueryListService reservationQueryListService;
+
+    @GetMapping
 }
---
+++ src/main/java/com/command/toyvillage_server/domain/app/reservation/service/ReservationQueryListService.java
@@ -0,0 +1,24 @@
+package com.command.toyvillage_server.domain.app.reservation.service;
+
+import com.command.toyvillage_server.domain.app.reservation.domain.Reservation;
+import com.command.toyvillage_server.domain.app.reservation.domain.repository.ReservationRepository;
+import com.command.toyvillage_server.domain.app.reservation.presentation.dto.response.ReservationListResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReservationQueryListService {
+    private final ReservationRepository reservationRepository;
+
+    @Transactional(readOnly = true)
+    public List<ReservationListResponse> execute() {
+        return reservationRepository.findAll()
+            .stream()
+            .map(ReservationListResponse::of)
+            .toList();
+    }
+}
