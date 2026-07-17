caac17491c8943fdc09c45c237458345297372c6
oijwef098234 <oijwef098234@gmail.com>
2026-07-18 02:50:50 +0900

    feat(#82) :: 유저 권한 부여 메서드 controller 매핑

--- src/main/java/com/command/toyvillage_server/domain/app/reservation/presentation/ReservationController.java
+++ src/main/java/com/command/toyvillage_server/domain/app/reservation/presentation/ReservationController.java
@@ -2,6 +2,7 @@ package com.command.toyvillage_server.domain.app.reservation.presentation;
 
 import com.command.toyvillage_server.domain.app.reservation.presentation.dto.response.ReservationListResponse;
 import com.command.toyvillage_server.domain.app.reservation.presentation.dto.response.ReservationResponse;
+import com.command.toyvillage_server.domain.app.reservation.service.ReservationPermissionSettingService;
 import com.command.toyvillage_server.domain.app.reservation.service.ReservationQueryListService;
 import com.command.toyvillage_server.domain.app.reservation.service.ReservationQueryService;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,7 @@ import java.util.List;
 public class ReservationController {
     private final ReservationQueryService reservationQueryService;
     private final ReservationQueryListService reservationQueryListService;
+    private final ReservationPermissionSettingService reservationPermissionSettingService;
 
     @GetMapping("/{id}")
     public ReservationResponse getDetail(@PathVariable Long id) {
@@ -25,4 +27,10 @@ public class ReservationController {
     public List<ReservationListResponse> getList() {
         return reservationQueryListService.execute();
     }
+
+    @PostMapping("/permission/{id}")
+    public void setReservationPermission(@PathVariable Long id, @RequestBody boolean reservationPermission) {
+        reservationPermissionSettingService.execute(id, reservationPermission);
+    }
+
 }
