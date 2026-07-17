226427fec3fb367843382ed27fb06c4288202e28
oijwef098234 <oijwef098234@gmail.com>
2026-07-18 02:16:13 +0900

    feat(#82) :: 유저 전체조회 메서드 controller 매핑

---
+++ src/main/java/com/command/toyvillage_server/domain/app/user/presentation/UserController.java
@@ -0,0 +1,20 @@
+package com.command.toyvillage_server.domain.app.user.presentation;
+
+import com.command.toyvillage_server.domain.app.user.presentation.dto.response.UserResponse;
+import com.command.toyvillage_server.domain.app.user.service.UserQueryListService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserQueryListService userQueryListService;
+
+    public List<UserResponse> getUserList(){
+        return userQueryListService.execute();
+    }
+}
