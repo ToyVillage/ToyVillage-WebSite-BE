13a5eb07be7fc16c386ab3bfcff635a6607aac98
Kanghee <kh63167574@gmail.com>
2026-07-26 12:03:25 +0900

    refactor : 안내이름 수정

--- src/main/java/com/command/toyvillage_server/domain/app/team/presentation/dto/request/TeamRequest.java
+++ src/main/java/com/command/toyvillage_server/domain/app/team/presentation/dto/request/TeamRequest.java
@@ -3,7 +3,7 @@ package com.command.toyvillage_server.domain.app.team.presentation.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record TeamRequest(
-        @NotBlank(message = "팀 이름을 입력해주세요.")
+        @NotBlank(message = "공백으로 둘 순 없습니다.")
         String name
 ) {
 }
