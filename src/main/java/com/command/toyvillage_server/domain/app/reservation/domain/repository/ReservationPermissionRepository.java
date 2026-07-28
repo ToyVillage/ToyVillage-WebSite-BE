3a8947ede6e38a3ed8b6acac64dafb6022873ab1
oijwef098234 <oijwef098234@gmail.com>
2026-07-28 20:26:32 +0900

    feat(#98) :: 단체예약 권한 조회 메서드 구현

--- src/main/java/com/command/toyvillage_server/domain/app/reservation/domain/repository/ReservationPermissionRepository.java
+++ src/main/java/com/command/toyvillage_server/domain/app/reservation/domain/repository/ReservationPermissionRepository.java
@@ -2,6 +2,7 @@ package com.command.toyvillage_server.domain.app.reservation.domain.repository;
 
 import com.command.toyvillage_server.domain.app.reservation.domain.Reservation;
 import com.command.toyvillage_server.domain.app.reservation.domain.ReservationPermission;
+import com.command.toyvillage_server.domain.common.auth.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,8 +11,11 @@ import java.util.List;
 
 public interface ReservationPermissionRepository extends JpaRepository<ReservationPermission, Long> {
     boolean existsByReservationIdAndUserId(Long reservationId, Long userId);
+
     void deleteByReservationIdAndUserId(Long reservationId, Long userId);
 
     @Query("select rp.reservation from ReservationPermission rp where rp.user.id = :userId")
     List<Reservation> findReservationsByUserId(@Param("userId") Long userId);
+
+    List<User> findAllByReservationId(Long reservationId);
 }
--- src/main/java/com/command/toyvillage_server/domain/app/reservation/service/ReservationPermissionQueryListService.java
+++ src/main/java/com/command/toyvillage_server/domain/app/reservation/service/ReservationPermissionQueryListService.java
@@ -1,11 +1,38 @@
 package com.command.toyvillage_server.domain.app.reservation.service;
 
+import com.command.toyvillage_server.domain.app.reservation.domain.Reservation;
+import com.command.toyvillage_server.domain.app.reservation.domain.repository.ReservationPermissionRepository;
+import com.command.toyvillage_server.domain.app.reservation.domain.repository.ReservationRepository;
+import com.command.toyvillage_server.domain.app.reservation.exception.ReservationNotFoundException;
+import com.command.toyvillage_server.domain.app.reservation.presentation.dto.response.ReservationPermissionResponse;
+import com.command.toyvillage_server.domain.common.auth.user.domain.User;
+import com.command.toyvillage_server.domain.common.auth.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ReservationPermissionQueryListService {
+    private final ReservationRepository reservationRepository;
+    private final UserRepository userRepository;
+    private final ReservationPermissionRepository reservationPermissionRepository;
 
 
+    public List<ReservationPermissionResponse> execute(Long reservationId) {
+        Reservation reservation = reservationRepository.findById(reservationId)
+            .orElseThrow(() -> ReservationNotFoundException.EXCEPTION);
+
+        List<User> users = reservationPermissionRepository.findAllByReservationId(reservationId);
+
+        List<ReservationPermissionResponse> responses = new ArrayList<>();
+        for(User user : users) {
+            ReservationPermissionResponse response = ReservationPermissionResponse.of(user.getName());
+            responses.add(response);
+        }
+
+        return responses;
+    }
 }
