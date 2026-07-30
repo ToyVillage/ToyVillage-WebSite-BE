4e95fd35fc0beb1db9b21928699cb6936c6eeaf5
oijwef098234 <oijwef098234@gmail.com>
2026-07-30 15:44:20 +0900

    feat(#98) :: 불필요한 공백 제거

--- src/main/java/com/command/toyvillage_server/domain/app/reservation/domain/repository/ReservationPermissionRepository.java
+++ src/main/java/com/command/toyvillage_server/domain/app/reservation/domain/repository/ReservationPermissionRepository.java
@@ -5,7 +5,6 @@ import com.command.toyvillage_server.domain.app.reservation.domain.ReservationPe
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.util.List;
 import java.util.Optional;
 
