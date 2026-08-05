4b87509e2ed573904861c993b4348699bec9bcd9
oijwef098234 <oijwef098234@gmail.com>
2026-08-05 17:24:06 +0900

    feat(#105) :: 사전 답사 전 데이터 개수 구하는 메서드 구현

--- src/main/java/com/command/toyvillage_server/domain/app/reservation/domain/repository/ReservationRepository.java
+++ src/main/java/com/command/toyvillage_server/domain/app/reservation/domain/repository/ReservationRepository.java
@@ -2,10 +2,16 @@ package com.command.toyvillage_server.domain.app.reservation.domain.repository;
 
 import com.command.toyvillage_server.domain.app.reservation.domain.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     Optional<Reservation> findById(Long id);
+
     List<Reservation> findAll();
+
+    int countByVisitSiteDateBefore(LocalDate date);
 }
--- src/main/java/com/command/toyvillage_server/domain/app/reservation/service/admin/ReservationAdminQueryListService.java
+++ src/main/java/com/command/toyvillage_server/domain/app/reservation/service/admin/ReservationAdminQueryListService.java
@@ -1,9 +1,12 @@
 package com.command.toyvillage_server.domain.app.reservation.service.admin;
 
 import com.command.toyvillage_server.domain.app.reservation.domain.repository.ReservationRepository;
+import com.command.toyvillage_server.domain.app.reservation.presentation.dto.response.ReservationAdminQueryListResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
@@ -11,5 +14,8 @@ public class ReservationAdminQueryListService {
     private final ReservationRepository reservationRepository;
 
     @Transactional
-    public
+    public ReservationAdminQueryListResponse execute() {
+        int beforeVisitSite = reservationRepository.countByVisitSiteDateBefore(LocalDate.now());
+        int doneVisitSite = 
+    }
 }
