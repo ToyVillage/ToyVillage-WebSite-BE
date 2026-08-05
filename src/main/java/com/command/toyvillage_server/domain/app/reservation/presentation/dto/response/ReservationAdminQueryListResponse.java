8fb876c9dbff9cc29919a8770970db37f328b841
oijwef098234 <oijwef098234@gmail.com>
2026-08-05 17:16:34 +0900

    feat(#105) :: 관리자 단체예약 전체조회 DTO 구현

--- src/main/java/com/command/toyvillage_server/domain/app/reservation/presentation/dto/response/ReservationAdminQueryListResponse.java
+++ src/main/java/com/command/toyvillage_server/domain/app/reservation/presentation/dto/response/ReservationAdminQueryListResponse.java
@@ -1,9 +1,27 @@
 package com.command.toyvillage_server.domain.app.reservation.presentation.dto.response;
 
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
 public record ReservationAdminQueryListResponse(
     int beforeVisitSite,
     int doneVisitSite,
     int doneVisit,
-
+    List<ReservationAdminQueryListObjectResponse> reservationAdminQueryListObjectResponse
     ) {
+    public static ReservationAdminQueryListResponse of(
+        int beforeVisitSite,
+        int doneVisitSite,
+        int doneVisit,
+        List<ReservationAdminQueryListObjectResponse> reservationAdminQueryListObjectResponse
+    ) {
+        return ReservationAdminQueryListResponse.builder()
+            .beforeVisitSite(beforeVisitSite)
+            .doneVisitSite(doneVisitSite)
+            .doneVisit(doneVisit)
+            .reservationAdminQueryListObjectResponse(reservationAdminQueryListObjectResponse)
+            .build();
+    }
 }
