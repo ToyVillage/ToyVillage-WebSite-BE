76693ddf41d133e4d8e569621b215d6546ee6149
oijwef098234 <oijwef098234@gmail.com>
2026-08-05 14:36:06 +0900

    feat(#105) :: 단일조회 DTO Entity mapping

--- src/main/java/com/command/toyvillage_server/domain/app/reservation/domain/Reservation.java
+++ src/main/java/com/command/toyvillage_server/domain/app/reservation/domain/Reservation.java
@@ -5,6 +5,8 @@ import lombok.AccessLevel;
 import lombok.CustomLog;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Currency;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -25,26 +27,17 @@ public class Reservation {
     @Column(nullable = false, name = "reservation_name")
     private String reservationName;
 
-    @Column(nullable = false, name = "leader_count")
-    private Integer leaderCount;
-
     @Column(nullable = false, name = "reservation_count")
     private Integer reservationCount;
 
-    @Column(nullable = false, name = "visit_site_count")
-    private Integer visitSiteCount;
+    @Column(nullable = false, name = "reservation_date")
+    private LocalDate reservationDate;
 
-    @Column(nullable = false)
-    private String location;
-
-    @Column(nullable = false, name = "visit_date")
-    private LocalDateTime visitDate;
-
-    @Column(nullable = false, name = "out_time")
-    private LocalTime exitTime;
+    @Column(nullable = false, name = "reservation_time")
+    private LocalTime reservationTime;
 
     @Column(nullable = false, name = "visit_site_date")
-    private LocalDateTime visitSiteDate;
+    private LocalDate visitSiteDate;
 
     @Column(nullable = false, name = "visit_site_time")
     private LocalTime visitSiteTime;
@@ -52,8 +45,23 @@ public class Reservation {
     @Column(nullable = false, name = "visit_site_exit_time")
     private LocalTime visitSiteExitTime;
 
-    @Column(nullable = false, name = "reservation_date")
-    private LocalDate reservationDate;
+    @Column(nullable = false, name = "visit_site_count")
+    private Integer visitSiteCount;
+
+    @Column(nullable = false, name = "leader_count")
+    private Integer leaderCount;
+
+    @Column(nullable = false, name = "visit_date")
+    private LocalDate visitDate;
+
+    @Column(nullable = false, name = "visit_time")
+    private LocalTime visitTime;
+
+    @Column(nullable = false, name = "out_time")
+    private LocalTime exitTime;
+
+    @Column(nullable = false)
+    private String location;
 
     @Column(nullable = false)
     private Integer money;
--- src/main/java/com/command/toyvillage_server/domain/app/reservation/presentation/dto/response/ReservationResponse.java
+++ src/main/java/com/command/toyvillage_server/domain/app/reservation/presentation/dto/response/ReservationResponse.java
@@ -4,39 +4,42 @@ import com.command.toyvillage_server.domain.app.reservation.domain.Reservation;
 import lombok.Builder;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @Builder
 public record ReservationResponse(
     Long id,
     String reservationName,
-    Integer leaderCount,
     Integer reservationCount,
     LocalDate reservationDate,
     LocalTime reservationTime,
-    String location,
-    LocalDateTime visitDate,
-    LocalTime exitTime,
-    LocalDateTime visitSiteDate,
-    LocalTime visitTime,
+    LocalDate visitSiteDate,
+    LocalTime visitSiteTime,
     LocalTime visitSiteExitTime,
     Integer visitSiteCount,
+    LocalDate visitDate,
+    LocalTime visitTime,
+    LocalTime exitTime,
+    Integer leaderCount,
+    String location,
     Integer money
 ) {
     public static ReservationResponse from(Reservation reservation) {
         return ReservationResponse.builder()
             .id(reservation.getId())
             .reservationName(reservation.getReservationName())
-            .leaderCount(reservation.getLeaderCount())
             .reservationCount(reservation.getReservationCount())
-            .location(reservation.getLocation())
-            .visitDate(reservation.getVisitDate())
-            .exitTime(reservation.getExitTime())
+            .reservationDate(reservation.getReservationDate())
+            .reservationTime(reservation.getReservationTime())
             .visitSiteDate(reservation.getVisitSiteDate())
-            .visitTime(reservation.getVisitSiteTime())
+            .visitSiteTime(reservation.getVisitSiteTime())
             .visitSiteExitTime(reservation.getVisitSiteExitTime())
             .visitSiteCount(reservation.getVisitSiteCount())
+            .visitDate(reservation.getVisitDate())
+            .visitTime(reservation.getVisitTime())
+            .exitTime(reservation.getExitTime())
+            .leaderCount(reservation.getLeaderCount())
+            .location(reservation.getLocation())
             .money(reservation.getMoney())
             .build();
     }
