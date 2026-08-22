8d089eb91a70c7a8d81398e4f963fe8e75ea48bc
oijwef098234 <oijwef098234@gmail.com>
2026-08-23 02:27:48 +0900

    feat(#112) :: 업무일지 질문 DTO 구현

--- src/main/java/com/command/toyvillage_server/domain/app/work_log/presentation/dto/request/WorkLogTemplateCreateRequest.java
+++ src/main/java/com/command/toyvillage_server/domain/app/work_log/presentation/dto/request/WorkLogTemplateCreateRequest.java
@@ -1,13 +1,18 @@
 package com.command.toyvillage_server.domain.app.work_log.presentation.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import java.util.List;
 
 public record WorkLogTemplateCreateRequest(
         @NotBlank(message = "업무일지 템플릿 제목을 비워둘 수 없습니다.")
         @Size(max = 100, message = "업무일지 템플릿 제목은 100자 이하여야 합니다.")
         String templateTitle,
 
-        List<>
+        @NotEmpty(message = "질문을 생성해주세요.")
+        List<WorkLogTemplateQuestionResponse> questions
 ) {
 }
