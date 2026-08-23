f235c08b5805b5b9a5eb17b665807e16d8a655f0
oijwef098234 <oijwef098234@gmail.com>
2026-08-23 17:01:03 +0900

    feat(#112) :: 업무일지 전체조회 DTO 구현

--- src/main/java/com/command/toyvillage_server/domain/app/work_log/presentation/dto/response/WorkLogTemplateResponse.java
+++ src/main/java/com/command/toyvillage_server/domain/app/work_log/presentation/dto/response/WorkLogTemplateResponse.java
@@ -1,7 +1,22 @@
 package com.command.toyvillage_server.domain.app.work_log.presentation.dto.response;
 
+import com.command.toyvillage_server.domain.app.work_log.domain.WorkLogTemplateQuestion;
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
 public record WorkLogTemplateResponse(
     String questionTitle,
-
+    List<WorkLogTemplateQuestionResponse> questionList
 ) {
+    public static WorkLogTemplateResponse of(
+        String questionTitle,
+        List<WorkLogTemplateQuestionResponse> questionList
+    ) {
+        return WorkLogTemplateResponse.builder()
+            .questionTitle(questionTitle)
+            .questionList(questionList)
+            .build();
+    }
 }
