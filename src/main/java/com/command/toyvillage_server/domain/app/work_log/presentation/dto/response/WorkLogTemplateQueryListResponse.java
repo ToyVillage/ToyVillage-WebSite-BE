3ed3cd14d4a15a7ef3491f4159733a8f5e6cd5b8
oijwef098234 <oijwef098234@gmail.com>
2026-08-23 18:50:34 +0900

    feat(#112) :: 업무일지 템플릿 전체조회 메서드 구현

--- src/main/java/com/command/toyvillage_server/domain/app/work_log/presentation/dto/response/WorkLogTemplateQueryListResponse.java
+++ src/main/java/com/command/toyvillage_server/domain/app/work_log/presentation/dto/response/WorkLogTemplateQueryListResponse.java
@@ -1,7 +1,5 @@
 package com.command.toyvillage_server.domain.app.work_log.presentation.dto.response;
 
-import com.command.toyvillage_server.domain.app.work_log.domain.WorkLogTemplate;
-import com.command.toyvillage_server.domain.app.work_log.domain.WorkLogTemplateQuestion;
 import lombok.Builder;
 
 import java.util.List;
--- src/main/java/com/command/toyvillage_server/domain/app/work_log/service/WorkLogTemplateQueryListService.java
+++ src/main/java/com/command/toyvillage_server/domain/app/work_log/service/WorkLogTemplateQueryListService.java
@@ -1,10 +1,15 @@
 package com.command.toyvillage_server.domain.app.work_log.service;
 
+import com.command.toyvillage_server.domain.app.work_log.domain.WorkLogTemplate;
 import com.command.toyvillage_server.domain.app.work_log.domain.repository.WorkLogTemplateQuestionRepository;
 import com.command.toyvillage_server.domain.app.work_log.domain.repository.WorkLogTemplateRepository;
+import com.command.toyvillage_server.domain.app.work_log.presentation.dto.response.WorkLogTemplateQueryListResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +18,13 @@ public class WorkLogTemplateQueryListService {
     private final WorkLogTemplateQuestionRepository workLogTemplateQuestionRepository;
 
     @Transactional(readOnly = true)
-    public
+    public WorkLogTemplateQueryListResponse execute() {
+        List<WorkLogTemplate> workLogTemplateList = workLogTemplateRepository.findAll();
+
+        List<String> templateTitles = workLogTemplateList.stream()
+            .map(WorkLogTemplate::getTemplateTitle)
+            .toList();
+
+        return WorkLogTemplateQueryListResponse.from(templateTitles);
+    }
 }
