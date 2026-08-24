1de0c545031645cd17c94cf5c19927d954df05c6
oijwef098234 <oijwef098234@gmail.com>
2026-08-25 02:31:33 +0900

    feat(#117) :: 단체예약 검색 쿼리 및 직원 조회 메서드 추가

--- src/main/java/com/command/toyvillage_server/domain/app/auth/admin/domain/repository/AppAdminRepository.java
+++ src/main/java/com/command/toyvillage_server/domain/app/auth/admin/domain/repository/AppAdminRepository.java
@@ -1,12 +1,16 @@
 package com.command.toyvillage_server.domain.app.auth.admin.domain.repository;
 
 import com.command.toyvillage_server.domain.app.auth.admin.domain.AppAdmin;
+import com.command.toyvillage_server.domain.app.auth.admin.domain.AppAdminRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AppAdminRepository extends JpaRepository<AppAdmin, Long> {
     Optional<AppAdmin> findByUsername(String username);
 
     boolean existsByUsername(String username);
+
+    List<AppAdmin> findByRoleAndNameContainingOrderByNameAsc(AppAdminRole role, String name);
 }
