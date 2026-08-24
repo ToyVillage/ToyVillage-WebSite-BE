24455e2c41b68b16768663247decee36aac03afc
oijwef098234 <oijwef098234@gmail.com>
2026-08-24 12:27:33 +0900

    feat(#112) :: 업무일지 양식 조회 기능 구현

--- src/main/java/com/command/toyvillage_server/domain/app/work_log/service/WorkLogTemplateQueryListService.java
+++ src/main/java/com/command/toyvillage_server/domain/app/work_log/service/WorkLogTemplateQueryListService.java
@@ -1,30 +1,27 @@
 package com.command.toyvillage_server.domain.app.work_log.service;
 
-import com.command.toyvillage_server.domain.app.work_log.domain.WorkLogTemplate;
-import com.command.toyvillage_server.domain.app.work_log.domain.repository.WorkLogTemplateQuestionRepository;
 import com.command.toyvillage_server.domain.app.work_log.domain.repository.WorkLogTemplateRepository;
+import com.command.toyvillage_server.domain.app.work_log.presentation.dto.response.WorkLogTemplateQueryListObjectResponse;
 import com.command.toyvillage_server.domain.app.work_log.presentation.dto.response.WorkLogTemplateQueryListResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class WorkLogTemplateQueryListService {
     private final WorkLogTemplateRepository workLogTemplateRepository;
-    private final WorkLogTemplateQuestionRepository workLogTemplateQuestionRepository;
 
     @Transactional(readOnly = true)
     public WorkLogTemplateQueryListResponse execute() {
-        List<WorkLogTemplate> workLogTemplateList = workLogTemplateRepository.findAll();
+        List<WorkLogTemplateQueryListObjectResponse> templates =
+            workLogTemplateRepository.findAllByOrderByIdDesc()
+                .stream()
+                .map(WorkLogTemplateQueryListObjectResponse::from)
+                .toList();
 
-        List<String> templateTitles = workLogTemplateList.stream()
-            .map(WorkLogTemplate::getTemplateTitle)
-            .toList();
-
-        return WorkLogTemplateQueryListResponse.from(templateTitles);
+        return WorkLogTemplateQueryListResponse.from(templates);
     }
 }
