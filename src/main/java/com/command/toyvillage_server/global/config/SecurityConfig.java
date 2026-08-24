3a9e3fe4b7bd73aa2d1dac0ce5784b453a7388f8
oijwef098234 <oijwef098234@gmail.com>
2026-08-24 12:27:33 +0900

    feat(#112) :: 업무일지 작성 및 수정 권한 설정 추가

--- src/main/java/com/command/toyvillage_server/global/config/SecurityConfig.java
+++ src/main/java/com/command/toyvillage_server/global/config/SecurityConfig.java
@@ -126,7 +126,8 @@ public class SecurityConfig {
                     .requestMatchers("/documents", "/documents/**").hasRole("APP_ADMIN")
 
                     // work log
-                    .requestMatchers(HttpMethod.POST, "/work-log/employee").hasRole("EMPLOYEE")
+                    .requestMatchers(HttpMethod.POST, "/work-log/employee/**").hasRole("EMPLOYEE")
+                    .requestMatchers(HttpMethod.PATCH, "/work-log/employee/**").hasRole("EMPLOYEE")
                     .requestMatchers(HttpMethod.GET, "/work-log/employee").hasRole("EMPLOYEE")
                     .requestMatchers(HttpMethod.GET, "/work-log").hasRole("APP_ADMIN")
                     .requestMatchers(HttpMethod.POST, "/work-log/template").hasRole("APP_ADMIN")
