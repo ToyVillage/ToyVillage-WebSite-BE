fbd19951ea222ba4976287518ed24224d2e4e12e
Kanghee <kh63167574@gmail.com>
2026-08-25 14:48:37 +0900

    refactor(#114) :: work 패키지 전체제거

--- src/main/java/com/command/toyvillage_server/domain/app/work/service/QueryWorkStaffService.java
+++
@@ -1,9 +0,0 @@
-package com.command.toyvillage_server.domain.app.work.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class QueryWorkStaffService {
-}
---
+++ src/main/java/com/command/toyvillage_server/domain/app/work_report/presentation/WorkReportController.java
@@ -0,0 +1,4 @@
+package com.command.toyvillage_server.domain.app.work_report.presentation;
+
+public class WorkReportController {
+}
