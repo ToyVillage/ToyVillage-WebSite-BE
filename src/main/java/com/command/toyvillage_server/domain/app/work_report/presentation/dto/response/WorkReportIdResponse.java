16c85c16213ba84c130c856053838109681af2db
Kanghee <kh63167574@gmail.com>
2026-08-27 01:21:16 +0900

    feat(#114) :: userfacade기능추가

---
+++ src/main/java/com/command/toyvillage_server/domain/app/work_report/presentation/dto/response/WorkReportIdResponse.java
@@ -0,0 +1,4 @@
+package com.command.toyvillage_server.domain.app.work_report.presentation.dto.response;
+
+public record WorkReportIdResponse() {
+}
---
+++ src/main/java/com/command/toyvillage_server/domain/app/work_report/service/WorkReportQueryService.java
@@ -0,0 +1,4 @@
+package com.command.toyvillage_server.domain.app.work_report.service;
+
+public class WorkReportQueryService {
+}
